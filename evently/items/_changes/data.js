5bcdf0887b9417edcdf8df315516155a6476bf14
Max Ogden <max@maxogden.com>
2010-06-21 23:59:24 -0700

    adding google static maps for food cart locations

--- evently/items/_changes/data.js
+++ evently/items/_changes/data.js
@@ -1,12 +1,16 @@
 function(data) {
-  $.log(data)
   var p;
+  
+  $.each(data.rows, function(index, item){
+    item.value.lat = item.value.geometry.coordinates[1];
+    item.value.lon = item.value.geometry.coordinates[0];
+  });
   var items = {
     items : data.rows.map(function(r) {
       p = r.value
       return p;
     })
-
   };
+
   return items;
 };
