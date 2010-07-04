4918e8cf093fde92b9f1cc794953c9f7d61c6bdc
Don Park <don@donpark.org>
2010-07-04 02:21:08 -0700

    switch to sammy based app

---
+++ _attachments/js/internetofthings.js
@@ -0,0 +1,43 @@
+(function($) {
+  
+  var app = $.sammy(function() {
+    this.element_selector = '#content';
+    this.use(Sammy.Template);
+
+    this.before(function() {
+    });
+
+    this.get('#/map', function(context) {
+      context.partial('cartmap.template', {id: 1}, function(rendered) {
+        context.$element().html(rendered);
+        var latlng = new google.maps.LatLng(45.597, -122.644);
+
+        var myOptions = {
+          zoom: 12,
+          center: latlng,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+      });
+    });
+    
+    this.get('#/carts', function(context) {
+      context.partial('carts.template', {id: 1}, function(rendered) {
+          context.$element().html(rendered);
+          $.couch.app(function(app) {
+            app.db.view('webapp/cart', {success: function(cart_results) {
+                                          $('#cartcount').html(cart_results.total_rows);
+                                          $.each(cart_results.rows, function(index,element) {
+                                            //context.partial('cart.template', {cart: element}, function(rendered) {
+                                            //   $('#carts ul').append(rendered);});
+                                          });
+                                       }});
+          });
+      });
+    });
+
+    $(function() {
+      app.run('#/map');
+    });
+  });
+})(jQuery);
